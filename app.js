a503ad1439cae6397c87574616d6ffcba0262894
Juan Nava <jnava9268@gmail.com>
2023-05-28 16:38:51 -0600

    Total change on design

--- app.js
+++ app.js
@@ -1,3 +1,50 @@
+// Function to show the loading section
+function showLoadingSection() {
+    document.getElementById('loading-section').style.display = 'flex';
+  }
+  
+  // Function to hide the loading section
+  function hideLoadingSection() {
+    document.getElementById('loading-section').style.display = 'none';
+  }
+  
+  // Function to start the loading animation
+  function startLoadingAnimation() {
+      var loadingText = document.getElementById('loading-text')
+      loadingText.textContent = "You've landed on a Portfolio webpage of a Software Developer";
+      setTimeout(function() {
+        loadingText.classList.remove('loading-text-animation')
+        var loadingText2 = document.getElementById('loading-text2')
+        loadingText2.classList.add('loading-text-animation')
+        loadingText2.textContent = "Loading repositories required to continue";
+      }, 4000);
+    setTimeout(function() {
+        window.location.hash = 'welcome-section' 
+        hideLoadingSection()
+    }, 9500); // Hide the loading section after 9 seconds
+  }
+  
+  // Call the function to start the loading animation
+  showLoadingSection();
+  startLoadingAnimation();
+
+/*//////////////////////////////////////////////////////*/
+  // Function to display the loading animation
+function displayLoadingAnimation() {
+    var loadingAnimation = document.getElementById('loading-animation');
+    var frames = ['\\', '|', '/', '-'];
+    var currentFrame = 0;
+  
+    setInterval(function() {
+      loadingAnimation.textContent = frames[currentFrame];
+      currentFrame = (currentFrame + 1) % frames.length;
+    }, 200); // Change the animation speed by adjusting the interval time
+  }
+  
+  // Call the function to display the loading animation
+  displayLoadingAnimation();
+/*//////////////////////////////////////////////////////*/
+
 function sendForm(){
 
     // Storing Field Values In Variables
@@ -35,22 +82,38 @@ function sendForm(){
 
 }
 
-window.addEventListener('DOMContentLoaded', () => {
+// Get all the links in the navbar
+var navbarLinks = document.querySelectorAll('#navbar a');
 
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-        const id = entry.target.getAttribute('id');
-        if (entry.intersectionRatio > 0) {
-            document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
-        } else {
-            document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
-        }
-        });
-    });
-    
-    // Track all sections that have an `id` applied
-    document.querySelectorAll('section[id]').forEach((section) => {
-        observer.observe(section);
-    });
-    
-    });
+// Function to handle scroll event
+function handleScroll() {
+  // Get the current scroll position
+  var scrollPosition = window.scrollY;
+
+  // Loop through each navbar link
+  navbarLinks.forEach(function(link) {
+    var sectionId = link.getAttribute('href');
+    var section = document.querySelector(sectionId);
+
+    if (section) {
+      // Get the top position and height of the section
+      var sectionTop = section.offsetTop;
+      var sectionHeight = section.offsetHeight;
+
+      // Check if the section is in the viewport
+      if (scrollPosition >= sectionTop && scrollPosition < (sectionTop + sectionHeight)) {
+        // Add the active class to the link
+        link.classList.add('active-link');
+      } else {
+        // Remove the active class from the link
+        link.classList.remove('active-link');
+      }
+    }
+  });
+}
+
+// Add scroll event listener
+window.addEventListener('scroll', handleScroll);
+
+// Call the handleScroll function initially
+handleScroll();
